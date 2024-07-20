e21ace94f90064e62282f30a6cb1a916771a6965
Minou Emmad <maemmad88@gmail.com>
2024-07-20 16:16:23 -0400

    data extraction of relevant information

--- data_extraction.py
+++ data_extraction.py
@@ -12,18 +12,27 @@ def fetch_data():
         sslmode='verify-ca',
         sslrootcert='./global-bundle.pem'
     )
+    
     query = """
     SELECT 
-        date,
-        exec_name,
-        transaction_type,
+        trade_date,
+        ticker,
+        company_name,
+        person_name,
+        owner_type,
+        num_shares,
         price_per_share,
-        total_value,
-        shares_following,
-        percent_change,
-        transaction_code
+        total_value_of_trade,
+        shares_owned_following_transaction,
+        percent_change_in_shares,
+        transaction_codes,
+        division,
+        major_group
     FROM processed_listings
+    ORDER BY trade_date DESC
+    LIMIT 500
     """
+    
     df = pd.read_sql_query(query, conn)
     conn.close()
     return df
