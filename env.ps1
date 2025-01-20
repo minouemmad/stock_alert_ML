bdd2b8072a225caf88b26db2095e3fb9e0c0b9f2
Minou Emmad <maemmad88@gmail.com>
2025-01-20 11:29:35 -0600

    Update .gitignore

--- env.ps1
+++
@@ -1,7 +0,0 @@
-$env:PUBLIC_VAPID_KEY="BA_2l9Xf777IAJfNCMmX5mbdlD1MJfJk9gGdl5DLwx6PsE68TwejSGhUBEBHzMLsT6vpGQ_uL2dHHGQXMsBs1Tg"
-$env:PRIVATE_VAPID_KEY="OIeLLOjfX4TctfVtP_0vdHe7gsQeQD77QTLeoI-ateU"
-$env:POSTGRES_ENDPOINT="stock-alert-test.cpomc2oq49pi.us-east-2.rds.amazonaws.com"
-$env:POSTGRES_PASSWORD='9B,z$W^nOuht$Zg'
-$env:READONLY_PASSWORD="superHardPasswordForReadonlyUser727"
-$env:GOOGLE_CLIENT_ID="976347904155-ncou8gskp4egvl0gup1ccon40pdipghe.apps.googleusercontent.com"
-$env:GOOGLE_CLIENT_SECRET="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"
